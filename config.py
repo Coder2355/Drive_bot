f54efead165f14a91a185a8748ea9cbd696a4ffb
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-03 15:20:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ import os
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
-GDRIVE_CREDENTIALS = os.getenv("GDRIVE_CREDENTIALS", "{"installed":{"client_id":"308189288149-le4t81fn25cm6p6nl4km0jdqlp0it6dv.apps.googleusercontent.com","project_id":"botsdeveloper","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost"]}}")
