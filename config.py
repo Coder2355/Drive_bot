7122b553832b16de15cba0bf2c2e0c2f2ab74719
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-03 09:43:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+import os
+
+API_ID = os.getenv("API_ID", "21740783")
+API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
+GDRIVE_CREDENTIALS = os.getenv("GDRIVE_CREDENTIALS", "client_secret_308189288149-le4t81fn25cm6p6nl4km0jdqlp0it6dv.apps.googleusercontent.com.json")
