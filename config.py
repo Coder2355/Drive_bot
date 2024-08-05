0d4337fdee4dc87bf5a7419be55967613bb62c90
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-05 21:24:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
 DOWNLOAD_DIR = "downloads"
 
-if not os.path.exists(DOWNLOAD_DIR):
-    os.makedirs(DOWNLOAD_DIR)
+# FFmpeg path (if it's not in your PATH, specify it here)
+FFMPEG_PATH = "/usr/bin/ffmpeg"  # Adjust based on your system
