481eeedd587f362c134350242fe087abebfcedea
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-05 18:49:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ import os
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
-DOWNLOAD_PATH = 'downloads'
-PROCESSED_PATH = 'processed'
+DOWNLOAD_DIR = "downloads"
+
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
