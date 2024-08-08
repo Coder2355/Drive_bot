998616849b87b6a4d13c882531120dc43c0c2a86
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-08 17:23:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,4 @@ import os
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
-DOWNLOAD_DIR = "downloads"
-
-# FFmpeg path (if it's not in your PATH, specify it here)
-FFMPEG_PATH = "/usr/bin/ffmpeg"  # Adjust based on your system
+PORT = int(os.getenv("PORT", 8000))
