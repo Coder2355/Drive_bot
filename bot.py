f85341c78eb3e1855b414ffde11955ef4aa5e7b1
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-25 14:30:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import sys
 import time
 import asyncio
 import logging 
-from config import API_ID, API_HASH, BOT_TOKEN
+import config
 from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, request, jsonify
 from pyrogram import Client, filters
