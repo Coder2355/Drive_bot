92fe132368931230d38db1d6beda7ba96e3f2ca4
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-27 13:26:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,10 @@
 
 import os
 
-class Config:
   
-     API_ID = os.getenv("API_ID", "21740783")
-     API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
-     BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
-     PORT = int(os.getenv("PORT", 8000))
-     DOWNLOAD_DIR = "./downloads"
+API_ID = os.getenv("API_ID", "21740783")
+API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
+PORT = int(os.getenv("PORT", 8000))
+DOWNLOAD_DIR = "./downloads"
 
