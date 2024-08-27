702164c774b1f2d56b056aad06f33422b5d83b31
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-27 13:25:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,119 +1,85 @@
 import os
 import asyncio
-import ffmpeg
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import Config
+import ffmpeg
+from config import API_ID, API_HASH, BOT_TOKEN, DOWNLOAD_DIR
 
-# Configure your bot
-API_ID = Config.API_ID
-API_HASH = Config.API_HASH
-BOT_TOKEN = Config.BOT_TOKEN
+# Initialize the bot with your API credentials
+app = Client("audio_merger_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# Create the bot client
-app = Client("audio_video_merger_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
 
-# Global variables to store the files temporarily
-merge_audio_files = {}
+# Store the first audio file's message details
+audio_storage = {}
 
-# Start command handler
-@app.on_message(filters.command("start"))
-async def start_message(client: Client, message: Message):
-    start_text = (
-        "👋 Hello! I'm the Audio+Video Merger Bot.\n\n"
-        "I can help you merge audio files together or merge an audio file with a video.\n\n"
-        "**Commands**:\n"
-        "🎵 `/merge_audio` - Reply to an audio file to start the audio merging process.\n"
-        "🎥 `/merge_video` - Reply to a video file to start the video + audio merging process.\n\n"
-        "Just send me the files, and I'll take care of the rest!"
-    )
-    await message.reply_text(start_text)
-
-# /merge_audio command handler
-@app.on_message(filters.command("merge_audio") & filters.reply & (filters.audio | filters.document))
+@app.on_message(filters.command("merge_audio") & filters.reply)
 async def merge_audio_command(client: Client, message: Message):
-    user_id = message.from_user.id
+    if message.reply_to_message.audio or message.reply_to_message.document:
+        # Send downloading message
+        downloading_message = await message.reply_text("Downloading the first audio...")
 
-    if user_id in merge_audio_files:
-        await message.reply_text("You already have a pending merge process. Please finish it first.")
-        return
+        # Download the first audio
+        audio_id = message.reply_to_message.message_id
+        first_audio_path = await client.download_media(message.reply_to_message, file_name=os.path.join(DOWNLOAD_DIR, f"audio1_{audio_id}.mp3"))
+        
+        # Store the path of the first audio and the chat_id
+        audio_storage[message.chat.id] = {
+            "first_audio": first_audio_path,
+            "user_id": message.from_user.id
+        }
+        
+        # Notify that the first audio has been downloaded
+        await downloading_message.edit_text("First audio downloaded. Please send the second audio file.")
 
-    if not message.reply_to_message or not (message.reply_to_message.audio or message.reply_to_message.document):
-        await message.reply_text("Please reply to the first audio file with the /merge_audio command.")
-        return
+@app.on_message(filters.audio | filters.document)
+async def process_second_audio(client: Client, message: Message):
+    if message.chat.id in audio_storage and audio_storage[message.chat.id]["user_id"] == message.from_user.id:
+        if message.audio or message.document:
+            # Send downloading message
+            downloading_message = await message.reply_text("Downloading the second audio...")
 
-    # Notify the user that downloading is starting
-    download_msg = await message.reply_text("Downloading the first audio file...")
+            # Download the second audio
+            audio_id = message.message_id
+            second_audio_path = await client.download_media(message, file_name=os.path.join(DOWNLOAD_DIR, f"audio2_{audio_id}.mp3"))
+            
+            # Notify that the second audio has been downloaded
+            await downloading_message.edit_text("Second audio downloaded. Merging the audios...")
 
-    # Download the first audio file
-    audio_file_path = await message.reply_to_message.download()
+            first_audio_path = audio_storage[message.chat.id]["first_audio"]
+            
+            # Merge the two audio files using FFmpeg
+            merged_audio_path = os.path.join(DOWNLOAD_DIR, f"merged_{message.chat.id}.mp3")
+            (
+                ffmpeg
+                .input(first_audio_path)
+                .input(second_audio_path)
+                .output(merged_audio_path, v="0", acodec="copy")
+                .run(overwrite_output=True)
+            )
+            
+            # Send uploading message
+            uploading_message = await message.reply_text("Uploading the merged audio...")
 
-    # Store the file path for merging
-    merge_audio_files[user_id] = {
-        "first_audio": audio_file_path,
-        "second_audio": None,
-        "output": f"merged_audio_{user_id}.mp3"
-    }
+            # Send the merged audio file
+            await client.send_audio(chat_id=message.chat.id, audio=merged_audio_path, caption="Here's your merged audio!")
+            
+            # Notify that the upload is complete
+            await uploading_message.delete()
 
-    # Update the user and remove the download message
-    await download_msg.delete()
-    await message.reply_text("First audio file downloaded. Now, send the second audio file to merge.")
+            # Clean up
+            os.remove(first_audio_path)
+            os.remove(second_audio_path)
+            os.remove(merged_audio_path)
+            del audio_storage[message.chat.id]
+        else:
+            await message.reply_text("Please send a valid audio file or document.")
+    else:
+        await message.reply_text("No merge process is active. Please start a new one with /merge_audio.")
 
-# Handle the second audio file
-@app.on_message((filters.audio | filters.document) & filters.user(lambda _, __, m: m.from_user.id in merge_audio_files))
-async def second_audio_handler(client: Client, message: Message):
-    user_id = message.from_user.id
+@app.on_message(filters.command("start"))
+async def start(client: Client, message: Message):
+    await message.reply_text("Hello! Reply to an audio file or document with /merge_audio to start the merging process.")
 
-    if user_id not in merge_audio_files:
-        return
-
-    if merge_audio_files[user_id]["second_audio"]:
-        await message.reply_text("You have already sent the second audio file. Please wait while I merge the files.")
-        return
-
-    # Notify the user that downloading is starting
-    download_msg = await message.reply_text("Downloading the second audio file...")
-
-    # Download the second audio file
-    second_audio_path = await message.download()
-
-    # Update the second audio file path in the dictionary
-    merge_audio_files[user_id]["second_audio"] = second_audio_path
-
-    # Update the user and remove the download message
-    await download_msg.delete()
-    await message.reply_text("Second audio file downloaded. Merging the audio files...")
-
-    # Paths
-    first_audio = merge_audio_files[user_id]["first_audio"]
-    second_audio = merge_audio_files[user_id]["second_audio"]
-    output_file = merge_audio_files[user_id]["output"]
-
-    # Merge audio files using FFmpeg
-    await merge_audio_files_ffmpeg(first_audio, second_audio, output_file)
-
-    # Notify the user that uploading is starting
-    upload_msg = await message.reply_text("Uploading the merged audio file...")
-
-    # Send the merged audio
-    await message.reply_audio(audio=output_file)
-
-    # Clean up and delete the upload message
-    await upload_msg.delete()
-
-    # Clean up
-    os.remove(first_audio)
-    os.remove(second_audio)
-    os.remove(output_file)
-
-    # Clear the stored data
-    merge_audio_files.pop(user_id)
-
-async def merge_audio_files_ffmpeg(first_audio, second_audio, output):
-    # Merge audio files using FFmpeg
-    await asyncio.to_thread(
-        ffmpeg.input(first_audio).input(second_audio).filter_complex('amix=inputs=2:duration=first:dropout_transition=2').output(output).run, overwrite_output=True
-    )
-
-if __name__ == "__main__":
-    app.run()
+app.run()
