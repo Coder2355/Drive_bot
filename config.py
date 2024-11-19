1ff659e595bae3a4b70d1e4df1425694c6145300
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-19 12:42:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import os
   
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7766709030:AAHsGisTBrmdF1G1jaXv1egIMIqe_UXRSxY")
 PORT = int(os.getenv("PORT", 8000))
 DOWNLOAD_DIR = "downloads/"
+SOURCE_CHANNEL_ID = "-1002183423252"
 
