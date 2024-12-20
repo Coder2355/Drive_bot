25ef3dbe2794366bc9c1e02a2b29ca7ecd46ec6e
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-12-20 22:12:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ import os
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7766709030:AAEHnIF6EkNttAij4cOCZat74PMK5Ymm6is")
-PORT = int(os.getenv("PORT", 8000))
+
+FILE_STORE_CHANNEL = -1002234974607  # Replace with your file store channel ID
+TARGET_CHANNEL = -1002245327685  # Replace with your target channel ID
 DOWNLOAD_DIR = "downloads/"
 SOURCE_CHANNEL_ID = "-1002183423252"
 
